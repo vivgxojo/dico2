eb9db6f82f39e449a7963cb44fc50790974f098f
Maryse M. <Maryse.Mongeau@cegepoutaouais.qc.ca>
2025-10-10 12:48:28 -0400

    pseudocode

--- dico2.py
+++ dico2.py
@@ -4,18 +4,28 @@ def simuler_attaque(j1: dict, j2: dict):
     :param j1: dictionnaire de stats du joueur 1
     :param j2: dictionnaire de stats du joueur 2
     """
-    dict_stats_j1["PV"] -= dict_stats_j2["Attaque"]
-    dict_stats_j2["PV"] -= dict_stats_j1["Attaque"]
+    dict_stats_j1["PV"] -= dict_stats_j2["Attaque"]    # si (attaque - défense) > 0
+    dict_stats_j2["PV"] -= dict_stats_j1["Attaque"]    # pv = pv - (attaque - défense)
 
 dict_stats_j1 = {   # TODO : ajouter la défense
     "Attaque" : 5,
+    "Defense" : 3,
     "PV" : 100
 }
 dict_stats_j2 = {
     "Attaque" : 3,
+    "Defense" : 7,
     "PV" : 100
 }
 # TODO : faire un boucle pour plusieurs attaques avec des valeurs aléatoires
+# boucle infinie
+    # générer attaque aléatoire pour chaque joueur
+    # mettres le valeurs d'attanques dans les dictionnaires
+    # appeler la fonction
+    # afficher l'état du jeu
+    # si un joueur 0 PV ou moins, terminer
+
+# Test - notes de cours
 dict_stats_j3 = dict_stats_j1 # pas une copie, ça reste le même dictionnaire
 dict_stats_j4 = dict_stats_j2.copy() # copie : créer un nouveau dictionnaire et copier les valeurs
 print(dict_stats_j1, dict_stats_j2)
